2d21615e37d4e642dbb0559be281854050ef7f82
didwogur28 <didwogur28@naver.com>
2023-03-09 18:06:49 +0900

    #4 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭 되도록 설계
    생성자와 'equals()', 'hashcode()' 등이 
    도메인 맥락에 맞게끔 디테일하게 설정

--- board-project/src/main/java/com/yjh/boardproject/domain/Article.java
+++ board-project/src/main/java/com/yjh/boardproject/domain/Article.java
@@ -1,16 +1,76 @@
 package com.yjh.boardproject.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
 
-    private LocalDateTime createAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Setter @Column(nullable = false) private String title;
+    @Setter @Column(nullable = false, length = 10000) private String content;
+
+    @Setter private String hashtag;
+
+    @ToString.Exclude   // 순환 참조로 인한 메모리 풀 에러를 방지하고자 해당 컬럼에서는 toString() 속성 출력을 제거
+    @OrderBy("id")
+    // @OneToMany : 1 대 다
+    // mappedBy = "article" -> 매핑 할 테이블 명을 세팅 해주지 않으면 새로운 매핑(매핑 할 두 테이블의 이름을 합친) 테이블을 만들어버림
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    // 동등성 검사
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;  // pattern matching
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
