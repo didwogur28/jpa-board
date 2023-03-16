d7e4e1c27fc0b1bb709dc8e718b86929a9e0ed65
didwogur28 <didwogur28@naver.com>
2023-03-16 17:24:48 +0900

    #6 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 `/login` 컨트롤러를 테스트.
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록
    `@Import` 추가

--- board-project/src/test/java/com/yjh/boardproject/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/yjh/boardproject/controller/ArticleControllerTest.java
@@ -1,17 +1,20 @@
 package com.yjh.boardproject.controller;
 
+import com.yjh.boardproject.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -27,7 +30,7 @@ class ArticleControllerTest {
         // Given
 
         // When & Then
-        mvc.perform(MockMvcRequestBuilders.get("/articles"))
+        mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
@@ -40,7 +43,7 @@ class ArticleControllerTest {
         // Given
 
         // When & Then
-        mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
+        mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
@@ -55,7 +58,7 @@ class ArticleControllerTest {
         // Given
 
         // when & Then
-        mvc.perform(MockMvcRequestBuilders.get("/articles/Search"))
+        mvc.perform(get("/articles/Search"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
@@ -68,7 +71,7 @@ class ArticleControllerTest {
         // Given
 
         // when & Then
-        mvc.perform(MockMvcRequestBuilders.get("/articles/Search-hashtag"))
+        mvc.perform(get("/articles/Search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
