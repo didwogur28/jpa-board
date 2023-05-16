fb426234012222ad588ead57b1362f97c5e24fd6
didwogur28 <didwogur28@naver.com>
2023-05-16 17:12:38 +0900

    #8 - 게시판, 게시글 뷰에 필요한 내용 추가

---
+++ board-project/src/main/java/com/yjh/boardproject/dto/request/ArticleRequest.java
@@ -0,0 +1,25 @@
+package com.yjh.boardproject.dto.request;
+
+import com.yjh.boardproject.dto.ArticleDto;
+import com.yjh.boardproject.dto.UserAccountDto;
+
+public record ArticleRequest(
+        String title,
+        String content,
+        String hashtag
+) {
+
+    public static ArticleRequest of(String title, String content, String hashtag) {
+        return new ArticleRequest(title, content, hashtag);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtag
+        );
+    }
+
+}
