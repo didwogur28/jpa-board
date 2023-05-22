0613d1c3e65863210337a12c3894cf4766a7f1de
didwogur28 <didwogur28@naver.com>
2023-05-22 20:39:11 +0900

    #18 - 패스워드 인코더가 인식할 수 있게 패스워드 정보 수정
    앞머리에 `{encoding-info}`와 같이 암호화 정보를 넣어줘야
    패스워드 인코더가 읽을 수 있다.
    이에 패스워드 데이터를 알맞게 수정함

--- board-project/src/main/resources/data.sql
+++ board-project/src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by)
-values ('yjh', 'asdf1234', 'Yjh', 'yjh@mail.com', 'I am Yjh.', now(), 'yjh', now(), 'yjh');
+values ('yjh', '{noop}asdf1234', 'Yjh', 'yjh@mail.com', 'I am Yjh.', now(), 'yjh', now(), 'yjh');
 
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by)
-values ('yjh2', 'asdf1234', 'Yjh2', 'yjh2@mail.com', 'I am Yjh2.', now(), 'yjh2', now(), 'yjh2');
+values ('yjh2', '{noop}asdf1234', 'Yjh2', 'yjh2@mail.com', 'I am Yjh2.', now(), 'yjh2', now(), 'yjh2');
 
 -- 123 게시글
 insert into article (user_id, title, content, hashtag, created_by, modified_by, created_at, modified_at) values
