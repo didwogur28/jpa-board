64d4333d94253ae8e83b846c6cb8b8252c045c77
didwogur28 <didwogur28@naver.com>
2023-05-22 20:39:40 +0900

    #18 - 수정, 삭제를 할 때 해당 사용자가 맞는지 확인하기 위한 응답 정보 추가
    인증 정보의 `userId`와
    해당 게시글/댓글의 작성자 `userId`가 동일한지 알려면
    응답에서 `userId`를 내려줘야 함
    이를 response dto 에 반영

--- board-project/src/main/java/com/yjh/boardproject/dto/response/ArticleWithCommentResponse.java
+++ board-project/src/main/java/com/yjh/boardproject/dto/response/ArticleWithCommentResponse.java
@@ -2,7 +2,6 @@ package com.yjh.boardproject.dto.response;
 
 import com.yjh.boardproject.dto.ArticleWithCommentsDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -16,11 +15,12 @@ public record ArticleWithCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        Set<ArticleCommentResponse> articleCommentResponses
-) implements Serializable {
+        String userId,
+        Set<ArticleCommentResponse> articleCommentsResponse
+) {
 
-    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
-        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
+    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, userId, articleCommentResponses);
     }
 
     public static ArticleWithCommentResponse from(ArticleWithCommentsDto dto) {
@@ -37,6 +37,7 @@ public record ArticleWithCommentResponse(
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
+                dto.userAccountDto().userId(),
                 dto.articleCommentDtos().stream()
                         .map(ArticleCommentResponse::from)
                         .collect(Collectors.toCollection(LinkedHashSet::new))
